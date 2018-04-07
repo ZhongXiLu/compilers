b19be00f714db583d5633605dc216f644ff1a15b
ZhongXiLu <zhongxilu@hotmail.com>
2018-04-07 16:12:06 +0200

    Added ParseTreeVisitor

---
+++ src/ParseTreeVisitor.py
@@ -0,0 +1,41 @@
+
+from graphviz import Digraph
+
+
+class ParseTreeVisitor:
+
+    def __init__(self):
+        self.nodes = {}
+        self.idCounter = 0
+
+    def generateDOT(self, parser, tree):
+        dot = Digraph("Parse Tree")
+
+        # Create root
+        id = self.createIDEntry(parser.ruleNames[tree.getRuleIndex()])
+        dot.node(str(id), self.nodes[id])
+
+        self.visit(dot, parser, tree, id)
+
+        dot.render("parse_tree.gv", view=True)
+
+    def createIDEntry(self, value):
+        id = self.idCounter
+        self.nodes[id] = value
+        self.idCounter += 1
+        return id
+
+    def visit(self, dot, parser, parseTree, id):
+
+        for child in parseTree.getChildren():
+
+            if child.getChildCount() == 0:  # terminal
+                childId = self.createIDEntry(child.getText())
+                dot.edge(str(id), str(childId))
+                dot.node(str(childId), self.nodes[childId])
+
+            else:
+                childId = self.createIDEntry(parser.ruleNames[child.getRuleIndex()])
+                dot.edge(str(id), str(childId))
+                dot.node(str(childId), self.nodes[childId])
+                self.visit(dot, parser, child, childId)
--- src/main.py
+++ src/main.py
@@ -3,14 +3,21 @@ import sys
 from antlr4 import *
 from CLexer import CLexer
 from CParser import CParser
- 
+from ParseTreeVisitor import ParseTreeVisitor
+
+
 def main(argv):
-    input = FileStream(argv[1])
+    inputFile = FileStream(argv[1])
     
-    lexer = CLexer(input)
+    lexer = CLexer(inputFile)
     stream = CommonTokenStream(lexer)
     parser = CParser(stream)
     tree = parser.prog()
- 
+
+    # Build AST with the generated parse tree
+    parseTreeVisitor = ParseTreeVisitor()
+    parseTreeVisitor.generateDOT(parser, tree)
+
+
 if __name__ == '__main__':
     main(sys.argv)
